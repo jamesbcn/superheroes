fa67604f9e026641f4788a9c9d3b3d20af055d38
jamesbcn <jamesquinlan@hotmail.co.uk>
2024-03-07 15:19:29 +0100

    Table clean-up and defunct custom pipe removed

--- src/app/custom-pipes/safe-data-source/safe-data-source.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SafeDataSourcePipe } from './safe-data-source.pipe';
-
-@NgModule({
-  declarations: [SafeDataSourcePipe],
-  exports: [SafeDataSourcePipe]
-})
-export class SafeDataSourceModule { }
