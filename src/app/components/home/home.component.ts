1e2196ad572f4772d55981c8f97874c667bbe64e
jamesbcn <jamesquinlan@hotmail.co.uk>
2024-03-07 14:44:12 +0100

    Table with heroes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { SuperheroesService } from '../../services/superheroes.service'
+import { Hero } from '../../models/hero';
 
 /**
  * @title Table with pagination
@@ -13,14 +15,31 @@ import {MatTableDataSource, MatTableModule} from '@angular/material/table';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  heroes: Hero[] = [];
+  displayedColumns: string[] = ['name', 'intelligence', 'strength', 'speed', 'durability', 'power', 'combat'];
+  dataSource = new MatTableDataSource<Hero>(this.heroes);
 
   @ViewChild(MatPaginator) paginator?: MatPaginator;
 
+  constructor(private superheroesService: SuperheroesService) { }
+
   ngAfterViewInit() {
-    if(this.paginator)
-    this.dataSource.paginator = this.paginator;
+    
+    this.superheroesService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+
+      if(this.paginator) {
+        this.dataSource = new MatTableDataSource<Hero>([...this.heroes]);
+
+        this.dataSource.data = [...this.heroes];
+        this.dataSource.paginator = this.paginator;
+
+        console.log(this)
+        
+      }
+
+    });
+
   }
 }
 
